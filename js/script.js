5997abc142dc37484d479c7341f855a8647ea63e
Thiago Daniel <39651883+Float07@users.noreply.github.com>
2021-10-27 10:51:00 -0300

    Interação com estrelas implementada

---
+++ js/script.js
@@ -0,0 +1,16 @@
+const starsBtn = document.getElementsByClassName("star-btn");
+
+for (let index = 0; index < starsBtn.length; index++) {
+    starsBtn[index].starNbr = index;
+    starsBtn[index].addEventListener('click', toggleStars);
+}
+
+function toggleStars(evt) {
+    for (var index = 0; index < evt.currentTarget.starNbr + 1; index++) {
+        starsBtn[index].querySelector(".star").src = "./assets/estrela_cheia.svg";
+    }
+
+    for (; index < starsBtn.length; index++) {
+        starsBtn[index].querySelector(".star").src = "./assets/estrela_vazia.svg";
+    }
+}
